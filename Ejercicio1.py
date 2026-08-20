c6d85ed3702de9a3a822bbbf10dff793d3458090
AnonymusAng <joan.erazo.z@uniautonoma.edu.co>
2026-08-20 14:56:02 -0500

    Update Ejercicio1.py

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -3,4 +3,4 @@ numero1 = float(input("Ingrese el primer numero: "))
 numero2 = float(input("Ingrese el segundo numero: "))
 numero3 = float(input("Ingrese el tercero numero: "))
 resultado = numero1+numero2+numero3
-print("El resultado de la suma es: ",resultado)
+print("El resultado de la suma  de los tres numeros es: ",resultado)
